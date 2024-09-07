99c959c27d297d5c4b14a61b61c228b7c4683ced
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2024-09-07 12:27:03 +0200

    clox start VM & stack

--- clox/build.zig
+++ clox/build.zig
@@ -5,11 +5,18 @@ pub fn build(b: *std.Build) void {
         .name = "clox",
         .target = b.host,
     });
-    exe.addCSourceFiles(.{ .files = &.{ "src/main.c", "src/chunk.c", "src/memory.c", "src/debug.c", "src/value.c" }, .flags = &.{"-std=c11"} });
+    exe.addCSourceFiles(.{
+        .files = &.{ "src/main.c", "src/chunk.c", "src/memory.c", "src/debug.c", "src/value.c", "src/vm.c" },
+        .flags = &.{
+            "-std=c11",
+            // "-gen-cdb-fragment-path", ".zig-cache/cdb"
+        },
+    });
     exe.linkSystemLibrary("c");
-    b.installArtifact(exe);
 
+    b.installArtifact(exe);
     const run_exe = b.addRunArtifact(exe);
+
     const run_step = b.step("run", "Run the application");
     run_step.dependOn(&run_exe.step);
 }
--- clox/src/debug.c
+++ clox/src/debug.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
 #include "debug.h"
+#include <stdio.h>
 
-void disassembleChunk(Chunk* chunk, const char* name) {
+void disassembleChunk(Chunk *chunk, const char *name) {
   printf("==%s==\n", name);
-  for(size_t offset = 0 ; offset < chunk->count ;) {
+  for (size_t offset = 0; offset < chunk->count;) {
     offset = disassembleInstruction(chunk, offset);
   }
 }
 
-static size_t constantInstruction(const char *name, Chunk *chunk, size_t offset) {
+static size_t constantInstruction(const char *name, Chunk *chunk,
+                                  size_t offset) {
   uint8_t constant = chunk->code[offset + 1];
   printf("%-16s %4d '", name, constant);
   printValue(chunk->constants.values[constant]);
@@ -21,22 +22,31 @@ static size_t simpleInstruction(const char *name, size_t offset) {
   return offset + 1;
 }
 
-size_t disassembleInstruction(Chunk* chunk, size_t offset) {
+size_t disassembleInstruction(Chunk *chunk, size_t offset) {
   printf("%04lu ", offset);
-  if (offset > 0 &&
-      chunk->lines[offset] == chunk->lines[offset - 1]) {
+  if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) {
     printf("   | ");
   } else {
     printf("%4d ", chunk->lines[offset]);
   }
   uint8_t instruction = chunk->code[offset];
-  switch(instruction) {
-    case OP_CONSTANT:
-      return constantInstruction("OP_CONSTANT", chunk, offset);
-    case OP_RETURN:
-      return simpleInstruction("OP_RETURN", offset);
-    default:
-      printf("Unknown opcode %d\n", instruction);
-      return offset + 1;
+  switch (instruction) {
+  case OP_CONSTANT:
+    return constantInstruction("OP_CONSTANT", chunk, offset);
+  case OP_NEGATE:
+    return simpleInstruction("OP_NEGATE", offset);
+  case OP_ADD:
+    return simpleInstruction("OP_ADD", offset);
+  case OP_SUBTRACT:
+    return simpleInstruction("OP_SUBTRACT", offset);
+  case OP_MULTIPLY:
+    return simpleInstruction("OP_MULTIPLY", offset);
+  case OP_DIVIDE:
+    return simpleInstruction("OP_DIVIDE", offset);
+  case OP_RETURN:
+    return simpleInstruction("OP_RETURN", offset);
+  default:
+    printf("Unknown opcode %d\n", instruction);
+    return offset + 1;
   }
 }
